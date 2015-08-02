18166dfe1f20d88d0b32b732f593299cb89810d5
unknown <richard.m@r-miles-lpt.Incubeta.lan>
2015-08-02 12:13:17 +0200

    removed modal for testing

--- footer.php
+++ footer.php
@@ -29,28 +29,7 @@
 		</div>
 	</div><!-- close .container -->
 </footer><!-- close #colophon -->
-<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-	Launch demo modal
-</button>
 
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title" id="myModalLabel">Modal title</h4>
-			</div>
-			<div class="modal-body">
-				...
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				<button type="button" class="btn btn-primary">Save changes</button>
-			</div>
-		</div>
-	</div>
-</div>
 <?php wp_footer(); ?>
 	
 </body>
